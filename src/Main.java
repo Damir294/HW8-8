6682ea2ab59166a8f6b031ea6781cfd44f5aa37b
Дамир <damir.obuhov@mail.ru>
2022-11-02 02:27:45 +0400

    решил

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public  class Main {
+    public static void main(String[] args) {
+
+        int masha = 67760;
+        int denis = 83690;
+        int kristina = 76230;
+        double percent = 0.1;
+        double newSalaryMasha = masha + (masha * percent);
+        double newSalaryDenis = denis + (denis * percent);
+        double newSalaryKristina = kristina + (kristina * percent);
+        int year = 12;
+        double salaryMashaForYear = (newSalaryMasha * year) - (masha * year) ;
+        double salaryDenisForYear = (newSalaryDenis * year) - (denis * year) ;
+        double salaryKristinaForYear = (newSalaryKristina * year) - (kristina * year) ;
+        System.out.println("Маша теперь получает " + newSalaryMasha + " рублей. Годовой доход вырос на " + salaryMashaForYear + " рублей");
+        System.out.println("Денис теперь получает " + newSalaryDenis + " рублей. Годовой доход вырос на " + salaryDenisForYear + " рублей");
+        System.out.println("Кристина теперь получает " + newSalaryKristina + " рублей. Годовой доход вырос на " + salaryKristinaForYear + " рублей");
+
+
+        int weight = 7000;
+        int weightLoss1 = 250;
+        int weightLoss2 = 500;
+        int days1 = weight  / weightLoss1;
+        System.out.println("Если спортсмен будет терять 250 грамм каждый день, на похудение уйдет " + days1 + " дней");
+        int days2 = weight / weightLoss2;
+        System.out.println("Если спортсмен будет терять 500 грамм каждый день, на похудение уйдет " + days2 + " дней");
+        int averageGram = (weightLoss1 + weightLoss2) / 2;
+        System.out.println("Среднее количество равно " + averageGram + " грамм");
+        int a = weight / averageGram;
+        int b = a % 1;
+        int averageDays = a - b;
+        System.out.println("Потребуется в среднем " + averageDays + " дней");
+
+    }
+}
